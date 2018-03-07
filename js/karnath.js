b0b2c00a28a9d5f50cfcb744c1a7d49081540db5
Jesus Cortez <jesus.cortez@mainstreethub.com>
2018-03-06 23:00:33 -0600

    slightly better background

--- js/karnath.js
+++ js/karnath.js
@@ -153,17 +153,3 @@ function set_unbanked_points(value) {
 	unbanked_points = value;
 	$("#unbanked_points").text(unbanked_points);
 }
-
-// deck.cards.forEach(function (card, i) {
-//                 card.setSide(Math.random() < 0.5 ? 'front' : 'back');
-
-//                 // explode
-//                 card.animateTo({
-//                     delay: 1000 + i * 2, // wait 1 second + i * 2 ms
-//                     duration: 500,
-//                     ease: 'quartOut',
-
-//                     x: Math.random() * window.innerWidth - window.innerWidth / 2,
-//                     y: Math.random() * window.innerHeight - window.innerHeight / 2
-//                 });
-//             });
